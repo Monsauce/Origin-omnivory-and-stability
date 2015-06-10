76a1cc27043422386f47cd0a0e50af98979dd845
Monica Granados <monica.granados@alumni.utoronto.ca>
2015-06-10 13:31:14 -0400

    finished LD75 models and added ANOVA and TukeyHSD output

--- Manuscript.R
+++ Manuscript.R
@@ -6,6 +6,9 @@ library(RCurl)
 snail.URL <- getURL("https://raw.githubusercontent.com/Monsauce/Size-does-matter-/master/Snail.csv")
 snail<-read.csv(text=snail.URL)
 
+#conver negatives to zeroes in Snail$Consumed 
+snail$Consumed[snail$Consumed<0]<- 0
+
 #pull out only crayfish treatments and order factors
 snail.subset<-snail[snail$Species%in%c("Limosus", "Propinquus","Rusticus ","Virilis "),]
 snail.subset$Species<- factor(snail.subset$Species, levels=c("Limosus","Rusticus ", "Propinquus", "Virilis "))
@@ -16,6 +19,8 @@ snail.subset<-snail.subset[snail.subset$Day!=49,]
 #pool across replicates
 snail.subset.mean<-ddply(.data=snail.subset, .variables=.(Day, Treatment, Species, Origin), .fun= summarise, Mean = mean(Density))
 
+#pool across replicates and day 
+snail.subset.mean.day<-ddply(.data=snail.subset, .variables=.(Treatment, Species, Origin, Replicate), .fun= summarise, Mean = mean(Density))
 
 #plot Figure S1 
 Figure.S1<-ggplot(snail.subset.mean, aes(x = Day, y = Mean))+geom_point()+facet_grid(Treatment~Species)+
@@ -23,35 +28,131 @@ Figure.S1<-ggplot(snail.subset.mean, aes(x = Day, y = Mean))+geom_point()+facet_
   theme_minimal()
 
 
-#calculate LD50 for each species and treatment 
+#calculate LD75 for each species and treatment 
 library(MASS)
 
 #Limosus, Omnivore 
 LO.snail<-snail.subset[snail.subset$Species=="Limosus" & snail.subset$Treatment %in% "Omnivore",] 
 attach(LO.snail)
-a=cbind(Alive,Consumed)
-LO.snail.output<-glm(a~Day, family=binomial, by="Replicate")
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
 
-dose.p(LO.snail.output,p=0.5)
+LO.snail.model.outputs<-ddply(LO.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+})  
+
+
+#Limosus, Predator 
+LP.snail<-snail.subset[snail.subset$Species=="Limosus" & snail.subset$Treatment %in% "Predator",] 
+attach(LP.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+LP.snail.model.outputs<-ddply(LP.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+})  
 
 
 #Rusticus, Omnivore 
 RO.snail<-snail.subset[snail.subset$Species=="Rusticus " & snail.subset$Treatment %in% "Omnivore",] 
-RO.snail.mean<-ddply(.data=RO.snail, .variables=.(Day, Treatment, Species), .fun= summarise, Alive = mean(Alive), Consumed=mean(Consumed))
-attach(RO.snail.mean)
-b=cbind(Alive,Consumed)
-RO.snail.output<-glm(b~Day, family=binomial)
+attach(RO.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
 
-dose.p(RO.snail.output,p=0.5)
-
-
-
-
+RO.snail.model.outputs<-ddply(RO.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
 
 
+#Rusticus, Predator 
+RP.snail<-snail.subset[snail.subset$Species=="Rusticus " & snail.subset$Treatment %in% "Predator",] 
+attach(RP.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+RP.snail.model.outputs<-ddply(RP.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
+
+
+#Propinquus, Omnivore
+PO.snail<-snail.subset[snail.subset$Species=="Propinquus" & snail.subset$Treatment %in% "Omnivore",] 
+attach(PO.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+PO.snail.model.outputs<-ddply(PO.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
+
+
+#Propinquus, Predator
+PP.snail<-snail.subset[snail.subset$Species=="Propinquus" & snail.subset$Treatment %in% "Predator",] 
+attach(PP.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+PP.snail.model.outputs<-ddply(PP.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
+
+
+#Virilis, Omnivore
+VO.snail<-snail.subset[snail.subset$Species=="Virilis " & snail.subset$Treatment %in% "Omnivore",] 
+attach(VO.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+VO.snail.model.outputs<-ddply(VO.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
+
+
+#Virilis, Predator
+VP.snail<-snail.subset[snail.subset$Species=="Virilis " & snail.subset$Treatment %in% "Predator",] 
+attach(VP.snail)
+model <- function(df){glm(cbind(Alive,Consumed)~Day, family=binomial, data = df)} 
+
+VP.snail.model.outputs<-ddply(VP.snail, "Replicate", function(x){
+  model.output <- model(x)
+  df.out <- dose.p(model.output,p=0.25)
+  return(df.out)
+}) 
+
+
+LD75.model.output <- rbind(LO.snail.model.outputs,RO.snail.model.outputs, PO.snail.model.outputs, VO.snail.model.outputs,
+                           LP.snail.model.outputs, RP.snail.model.outputs, PP.snail.model.outputs, VP.snail.model.outputs)
+
+LD75.model.output$Species<-snail.subset.mean.day$Species
+LD75.model.output$Trophic<-snail.subset.mean.day$Treatment
+colnames(LD75.model.output)[2] <- "LD75"
 
 
 
+LD75.model.output.mean<-ddply(.data=LD50.model.output, .variables=.(Species, Trophic), .fun= summarise, Mean = mean(LD50), SE=sd(LD50)/sqrt(length(LD50)))
+
+#plot Figure 1 
+Figure.1<-ggplot(LD75.model.output.mean, aes(x = Species, y = Mean, fill=Trophic))+
+  geom_bar(stat = "identity",position="dodge")+xlab("Species")+ylab("Day when 75% of snails consumed")+
+  scale_fill_manual(values=c("black", "grey"))+
+  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE),width=.2,position=position_dodge(.9))+
+  theme_minimal()
+
+#run two-way ANOVA and TukeyHSD to determine differences between species
+ANOVA1<-aov(LD75~Species*Trophic, data=LD75.model.output)
+TukeyHSD(ANOVA1)
 
 
-
+  
+  
+  
+  
+  
+  
